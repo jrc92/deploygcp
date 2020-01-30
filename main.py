d2d60bbb1080f30702d357a818daa1d60f27ee30
Jia Rong Chua <jiarongchua92@gmail.com>
2020-01-30 16:23:33 -0500

    trigger build

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World! My name is Jia :)"
+    return 'Hello World! My name is Jia!'
 
 @app.route('/name/<value>')
 def name(value):
